49c6e5312e0a73af7e7589025ada02e0a2ada734
SebastainBernal <sebasair2025@gmail.com>
2022-12-14 22:50:08 -0700

    commit tree 1302072e8c6a614dc96369e2fdaf5fdc1a3a36b1

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const names= [];
+
+function writeCards(names,events) {
+
+
+ for(let i=0;i<names.length;i++) {   
+names[i]=(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+
+}
+return names;
+
+}
+writeCards(['Guadalupe', 'Ollie', 'Aki', ],"surprise");
+
+let down=[11];
+
+function countDown(down){
+    debugger;
+    while(down >=0){
+        console.log(down--);
+}
+return down;
+}
+countDown(down);
